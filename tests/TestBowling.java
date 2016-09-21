bf44d50c1bb87c79649d6f086b169cb0e5e2984c
somename <someemail>
2016-09-21 14:49:24 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addFrame_5_5() {
+		BowlingGame game = new BowlingGame();
+		game.addFrame(new Frame(5,5));
+		int first = game.returnFrame().get(0).getFirstThrow();
+		int second = game.returnFrame().get(0).getSecondThrow();
+		assertEquals("10", first + second, 10);
+		
 	}
 
 }
