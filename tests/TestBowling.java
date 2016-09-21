fe7d9f48fd9dead8faa76912eba8d44df5c49c5b
somename <someemail>
2016-09-21 15:14:06 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -1,13 +1,23 @@
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TestBowling {
+	
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
 
 	@Test
 	public void addFrame_5_5() {
 		BowlingGame game = new BowlingGame();
-		game.addFrame(new Frame(5,5));
+		try {
+			game.addFrame(new Frame(5,5));
+		} catch (BowlingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		int first = game.getFrames().get(0).getFirstThrow();
 		int second = game.getFrames().get(0).getSecondThrow();
 		assertEquals("10", first + second, 10);
@@ -16,7 +26,12 @@ public class TestBowling {
 	@Test
 	public void calculateScoreOfAFrame_3_3() {
 		BowlingGame game = new BowlingGame();
-		game.addFrame(new Frame(3,3));
+		try {
+			game.addFrame(new Frame(3,3));
+		} catch (BowlingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		int score = game.getFrames().get(0).score();
 		assertEquals("6", score, 6);
 	}
@@ -25,11 +40,27 @@ public class TestBowling {
 	public void addTenFrames(){
 		BowlingGame game = new BowlingGame();
 		for(int i = 0; i < 10; i++) {
-			game.addFrame(new Frame(3,3));
+			try {
+				game.addFrame(new Frame(3,3));
+			} catch (BowlingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		int first = game.getFrames().get(0).score();
 		int last = game.getFrames().get(9).score();
 		assertEquals("12", first + last, 12);	
 	}
+	
+	@Test
+	public void addElevenFrames() throws Exception {
+		expectedEx.expect(BowlingException.class);
+		expectedEx.expectMessage("Game already has 10 frames");
+		BowlingGame game = new BowlingGame();
+		for(int i = 0; i < 10; i++) {
+			game.addFrame(new Frame(3,3));
+		}
+	}
+
 
 }
