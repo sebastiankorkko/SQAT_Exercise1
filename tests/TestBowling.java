efec13341fc1899b676daad01210f395a6ed1ce7
somename <someemail>
2016-09-21 14:53:32 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,10 +8,17 @@ public class TestBowling {
 	public void addFrame_5_5() {
 		BowlingGame game = new BowlingGame();
 		game.addFrame(new Frame(5,5));
-		int first = game.returnFrame().get(0).getFirstThrow();
-		int second = game.returnFrame().get(0).getSecondThrow();
+		int first = game.getFrames().get(0).getFirstThrow();
+		int second = game.getFrames().get(0).getSecondThrow();
 		assertEquals("10", first + second, 10);
-		
+	}
+	
+	@Test
+	public void calculateScoreOfAFrame_3_3() {
+		BowlingGame game = new BowlingGame();
+		game.addFrame(new Frame(3,3));
+		int score = game.getFrames().get(0).score();
+		assertEquals("6", score, 6);
 	}
 
 }
